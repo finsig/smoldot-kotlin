3021ae9f6a4a8910ab50841ec9836268e8d53baa
finsig <steve@finsig.io>
2024-07-31 21:32:09 -0400

    Update build_smoldotkotlin.sh

--- build_smoldotkotlin.sh
+++ build_smoldotkotlin.sh
@@ -4,13 +4,5 @@ source ".scripts/functions.sh"
 
 DEFAULT_VERSION='0.1.1'
 
-if [ "dev" = "$1" ]; then
-    log::message "Building development version"
-    /bin/zsh ".scripts/dev.sh" debug
-elif test -f ".scripts/$1.sh"; then
-    log::message "Building version $1"
-    /bin/zsh ".scripts/$1.sh" $2
-else
-    log::message "Building version $DEFAULT_VERSION"
-    /bin/zsh ".scripts/$DEFAULT_VERSION.sh" $1
-fi
+log::message "Building version $DEFAULT_VERSION"
+/bin/zsh ".scripts/$DEFAULT_VERSION.sh" $1
